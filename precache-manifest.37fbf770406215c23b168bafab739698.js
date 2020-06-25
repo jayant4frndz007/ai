223a9a485ffb7b8a77b632b19662f6b163b28ea4
jayant <jayant4frndz007@gmail.com>
2020-06-25 14:15:02 +0530

    Updates

---
+++ precache-manifest.37fbf770406215c23b168bafab739698.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66ee5f58d960e3d24b4106bf894dab4",
+    "url": "/ai/index.html"
+  },
+  {
+    "revision": "a255ecf55ad05977d30f",
+    "url": "/ai/static/css/main.a8b30ee7.chunk.css"
+  },
+  {
+    "revision": "72f51db76508c48130ef",
+    "url": "/ai/static/js/2.b1095209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ai/static/js/2.b1095209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a255ecf55ad05977d30f",
+    "url": "/ai/static/js/main.24b47d96.chunk.js"
+  },
+  {
+    "revision": "681d6080bb443c67eff6",
+    "url": "/ai/static/js/runtime-main.ad3b6d96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ai/static/media/logo.5d5d9eef.svg"
+  }
+]);
